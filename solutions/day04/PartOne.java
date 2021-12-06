4788841448e6128e3d0912162f3680ba48ad9876
Manjot1151 <manjothinarula@gmail.com>
2021-12-07 00:15:19 +0530

    day 04

---
+++ solutions/day04/PartOne.java
@@ -0,0 +1,73 @@
+package solutions.day04;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PartOne {
+    private static int[] nums;
+    private static List<int[][]> boards;
+    private static int boardSize = 5;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        parseInput();
+        for (int num : nums) {
+            for (int[][] board : boards) {
+                for (int i = 0; i < boardSize; i++) {
+                    for (int j = 0; j < boardSize; j++) {
+                        board[i][j] = (board[i][j] == num) ? -1 : board[i][j];
+                    }
+                }
+                if (isWinner(board)) {
+                    int sum = 0;
+                    for (int[] r : board) {
+                        for (int c : r) {
+                            if (c > 0) {
+                                sum += c;
+                            }
+                        }
+                    }
+                    System.out.printf("%d * %d = %d", sum, num, sum * num);
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    private static void parseInput() throws FileNotFoundException {
+        Scanner in = new Scanner(new File("inputs/day04"));
+        String[] numsStr = in.nextLine().split(",");
+        nums = new int[numsStr.length];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(numsStr[i]);
+        }
+        boards = new ArrayList<int[][]>();
+        while (in.hasNextLine()) {
+            int[][] board = new int[boardSize][boardSize];
+            for (int i = 0; i < boardSize; i++) {
+                for (int j = 0; j < boardSize; j++) {
+                    board[i][j] = in.nextInt();
+                }
+            }
+            boards.add(board);
+        }
+    }
+
+    private static boolean isWinner(int[][] board) {
+        for (int i = 0; i < boardSize; i++) {
+            boolean foundCol = true;
+            boolean foundRow = true;
+            for (int j = 0; j < boardSize; j++) {
+                if (board[i][j] != -1)
+                    foundCol = false;
+                if (board[j][i] != -1)
+                    foundRow = false;
+            }
+            if (foundRow || foundCol)
+                return true;
+        }
+        return false;
+    }
+}
