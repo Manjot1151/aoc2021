05f4721f7f8d7a76e170bf74160dec91e7b51390
Manjot1151 <manjothinarula@gmail.com>
2021-12-07 19:39:26 +0530

    day 07

--- solutions/day07/PartOne.java
+++ solutions/day07/PartOne.java
@@ -8,16 +8,14 @@ import java.util.Scanner;
 public class PartOne {
 
     // deviation about median is minimum
-    
-    public static void main(String[] args) throws FileNotFoundException
-    {
-        Scanner in = new Scanner(new File("inputs/day07")); 
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("inputs/day07"));
         int[] positions = Arrays.stream(in.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
         Arrays.sort(positions);
-        int median = positions[positions.length/2];
+        int median = positions[positions.length / 2];
         int deviation = 0;
-        for (int pos : positions)
-        {
+        for (int pos : positions) {
             deviation += Math.abs(pos - median);
         }
         System.out.println(deviation);
--- solutions/day07/PartTwo.java
+++ solutions/day07/PartTwo.java
@@ -7,27 +7,25 @@ import java.util.Scanner;
 
 public class PartTwo {
 
-    // on differentiating, it is found that fuel consumed is minimum when reaching within a range of mean +- 1
+    // on differentiating, it is found that fuel consumed is minimum when reaching
+    // within a range of mean +- 1
 
-    public static void main(String[] args) throws FileNotFoundException
-    {
-        Scanner in = new Scanner(new File("inputs/day07")); 
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("inputs/day07"));
         int[] nums = Arrays.stream(in.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
         int mean = 0;
-        for (int i : nums)
-        {
+        for (int i : nums) {
             mean += i;
         }
         mean /= nums.length;
-        System.out.println(Math.min(fuelConsumption(nums, mean), fuelConsumption(nums, mean+1)));
+        System.out.println(Math.min(fuelConsumption(nums, mean), fuelConsumption(nums, mean + 1)));
     }
-    private static int fuelConsumption(int[] nums, int pos)
-    {
+
+    private static int fuelConsumption(int[] nums, int pos) {
         int fuelConsumed = 0;
-        for (int i : nums)
-        {
+        for (int i : nums) {
             int deviation = pos - i;
-            fuelConsumed += (deviation * deviation + Math.abs(deviation))/2;
+            fuelConsumed += (deviation * deviation + Math.abs(deviation)) / 2;
         }
         return fuelConsumed;
     }
