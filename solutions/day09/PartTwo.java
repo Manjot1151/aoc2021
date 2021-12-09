2071cdb8e414f5ed8cef71438633839be9188a3b
Manjot1151 <manjothinarula@gmail.com>
2021-12-09 20:56:25 +0530

    optimized code

--- solutions/day09/PartTwo.java
+++ solutions/day09/PartTwo.java
@@ -24,29 +24,9 @@ public class PartTwo {
         for (int i = 0; i < grid.size(); i++) {
             int[] row = grid.get(i);
             for (int j = 0; j < row.length; j++) {
-                boolean isSafeCol = true;
-                boolean isSafeRow = true;
-                for (int k = (i == 0) ? i : i - 1; k <= i + 1 && k < grid.size(); k++) {
-                    if (k == i)
-                        continue;
-                    if (grid.get(k)[j] <= grid.get(i)[j]) {
-                        isSafeCol = false;
-                        break;
-                    }
-                }
-                if (!isSafeCol)
+                if (traversed.contains(i + " " + j))
                     continue;
-                for (int k = (j == 0) ? j : j - 1; k <= j + 1 && k < row.length; k++) {
-                    if (k == j)
-                        continue;
-                    if (grid.get(i)[k] <= grid.get(i)[j]) {
-                        isSafeRow = false;
-                        break;
-                    }
-                }
-                if (isSafeRow) {
-                    basins.add(getBasinSize(i, j));
-                }
+                basins.add(getBasinSize(i, j));
             }
         }
         Collections.sort(basins);
