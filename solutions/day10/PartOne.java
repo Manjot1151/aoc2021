7503a1ef3136961905c26ab8a2e39486a619e3e5
Manjot1151 <manjothinarula@gmail.com>
2021-12-10 11:10:19 +0530

    day 10

---
+++ solutions/day10/PartOne.java
@@ -0,0 +1,40 @@
+package solutions.day10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PartOne {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("inputs/day10"));
+        long errorSum = 0;
+        while (in.hasNextLine()) {
+            String syntax = in.nextLine();
+            List<Character> opened = new ArrayList<Character>();
+            for (char c : syntax.toCharArray()) {
+                if (c == '(' || c == '{' || c == '[' || c == '<') {
+                    opened.add(c);
+                } else {
+                    if (c == '}' || c == ']' || c == '>') {
+                        if (opened.get(opened.size() - 1) != c - 2) {
+                            errorSum += (c == ']') ? 57 : c == '}' ? 1197 : 25137;
+                            break;
+                        } else {
+                            opened.remove(opened.size() - 1);
+                        }
+                    } else if (c == ')') {
+                        if (opened.get(opened.size() - 1) != '(') {
+                            errorSum += 3;
+                            break;
+                        } else {
+                            opened.remove(opened.size() - 1);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(errorSum);
+    }
+}
---
+++ solutions/day10/PartTwo.java
@@ -0,0 +1,55 @@
+package solutions.day10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class PartTwo {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("inputs/day10"));
+        List<Long> scores = new ArrayList<Long>();
+        while (in.hasNextLine()) {
+            String syntax = in.nextLine();
+            List<Character> opened = new ArrayList<Character>();
+            for (char c : syntax.toCharArray()) {
+                if (c == '(' || c == '{' || c == '[' || c == '<') {
+                    opened.add(c);
+                } else {
+                    if (c == '}' || c == ']' || c == '>') {
+                        if (opened.get(opened.size() - 1) != c - 2) {
+                            opened.clear();
+                            break;
+                        } else {
+                            opened.remove(opened.size() - 1);
+                        }
+                    } else if (c == ')') {
+                        if (opened.get(opened.size() - 1) != '(') {
+                            opened.clear();
+                            break;
+                        } else {
+                            opened.remove(opened.size() - 1);
+                        }
+                    }
+                }
+            }
+            if (opened.size() != 0) {
+                long score = 0;
+                for (int i = opened.size() - 1; i >= 0; i--) {
+                    char c = opened.get(i);
+                    if (c == '{' || c == '[' || c == '<')
+                        c += 2;
+                    else
+                        c++;
+                    score *= 5;
+                    score += c == ')' ? 1 : c == ']' ? 2 : c == '}' ? 3 : 4;
+                }
+                scores.add(score);
+            }
+        }
+        Collections.sort(scores);
+        System.out.println(scores.get(scores.size() / 2));
+    }
+}
