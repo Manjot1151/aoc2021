098641a5ae6c5c3e8d7336101714d37ed9bbf83f
Manjot1151 <manjothinarula@gmail.com>
2021-12-14 13:27:16 +0530

    improved code

--- solutions/day14/PartTwo.java
+++ solutions/day14/PartTwo.java
@@ -22,30 +22,30 @@ public class PartTwo {
         Map<String, Long> pairFreq = new HashMap<>();
         for (int i = 0; i < poly.length() - 1; i++) {
             String pair = poly.substring(i, i + 2);
-            pairFreq.put(pair, pairFreq.getOrDefault(pair, 0l) + 1);
+            pairFreq.put(pair, pairFreq.getOrDefault(pair, 0L) + 1);
         }
         int steps = 40;
         while (steps-- > 0) {
             Map<String, Long> newPairFreq = new HashMap<>(pairFreq);
             for (String[] instr : instructions) {
-                if (pairFreq.getOrDefault(instr[0], 0l) == 0)
+                if (pairFreq.getOrDefault(instr[0], 0L) == 0)
                     continue;
-                newPairFreq.put(instr[0], newPairFreq.getOrDefault(instr[0], 0l) - pairFreq.getOrDefault(instr[0], 0l));
+                newPairFreq.put(instr[0], newPairFreq.getOrDefault(instr[0], 0L) - pairFreq.getOrDefault(instr[0], 0L));
                 String pair1 = instr[0].charAt(0) + instr[1];
                 String pair2 = instr[1] + instr[0].charAt(1);
-                long freq = pairFreq.getOrDefault(instr[0], 0l);
-                newPairFreq.put(pair1, newPairFreq.getOrDefault(pair1, 0l) + freq);
-                newPairFreq.put(pair2, newPairFreq.getOrDefault(pair2, 0l) + freq);
+                long freq = pairFreq.getOrDefault(instr[0], 0L);
+                newPairFreq.put(pair1, newPairFreq.getOrDefault(pair1, 0L) + freq);
+                newPairFreq.put(pair2, newPairFreq.getOrDefault(pair2, 0L) + freq);
             }
             pairFreq = newPairFreq;
         }
         Map<Character, Long> charFreq = new HashMap<>();
-        charFreq.put(poly.charAt(poly.length() - 1), 1l);
+        charFreq.put(poly.charAt(0), 1L);
         for (Map.Entry<String, Long> e : pairFreq.entrySet()) {
             if (e.getValue() == 0)
                 continue;
-            char c = e.getKey().charAt(0);
-            charFreq.put(c, charFreq.getOrDefault(c, 0l) + e.getValue());
+            char c = e.getKey().charAt(1);
+            charFreq.put(c, charFreq.getOrDefault(c, 0L) + e.getValue());
         }
         Collection<Long> frequencies = charFreq.values();
         System.out.println(Collections.max(frequencies) - Collections.min(frequencies));
